45535142c9705f6a94271170f83282d71c82bd17
Santiag0Vidal <david.vidal@est.fi.uncoma.edu.ar>
2024-10-02 14:33:23 -0300

    mas cosas

--- frontend/components/header.jsx/Header.jsx
+++ frontend/components/header.jsx/Header.jsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import Image from 'next/image'; // Importa el componente Image de Next.js
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons'; // Importa el ícono de buscador
 
@@ -12,24 +11,17 @@ const Header = () => {
     return (
         <header className="bg-blue-400 text-white p-4 flex justify-between items-center">
             <Link href="/HomeLog" passHref>
-                <div className="cursor-pointer">
-                    {/* Incluye el logo en lugar del texto "FoodBook" */}
-                    <Image 
-                        src="/logo.png" // Asegúrate de que el logo esté en la carpeta public con este nombre
-                        alt="FoodBook Logo"
-                        width={100} // Ajusta el tamaño según tus necesidades
-                        height={50}
-                    />
-                </div>
+                <h1 className="text-lg font-bold cursor-pointer">FoodBook</h1>
             </Link>
             <Link href="/search" passHref>
                 <div className="flex items-center space-x-2 cursor-pointer">
-                    <p className="text-black">Buscador</p>
+                <p className="text-black">Buscador</p>
                     <FontAwesomeIcon icon={faSearch} className="text-black" /> {/* Ícono de buscador */}
+                    
                 </div>
             </Link>
             <button onClick={handleLogout} className="bg-red-800 hover:bg-red-600 p-2 rounded">
-                <p className="text-white">Cerrar Sesión</p>   
+            <p className="text-white">Cerrar Sesión</p>   
             </button>
         </header>
     );
