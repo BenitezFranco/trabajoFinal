e8cc8ce8907b51cf92f6a3735a85ed9fd6c9ecad
Santiag0Vidal <david.vidal@est.fi.uncoma.edu.ar>
2024-11-26 14:36:33 -0300

    agrego icono de navegacion

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,8 +1,16 @@
-// pages/_app.js
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Foodbook</title> {/* Cambia el título según tu proyecto */}
+                <link rel="icon" href="/icono.png" type="image/png" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
